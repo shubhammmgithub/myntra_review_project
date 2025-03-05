aa22602ec6710a91f628e1acfdf7f0eda5f9e199
shubhammmgithub <shubhamrajbr28@gmail.com>
2025-03-06 01:15:49 +0530

    This is my first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+setup(
+    
+    name='Scrapper',
+    version='0.01',
+    author="ShubhamRaj",
+    author_email="shubhamrajbr28@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    
+)
